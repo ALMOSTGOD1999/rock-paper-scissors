6c50aa75ffd56b1d41d5b468a543b98fe59f4c90
Rahul Mukherjee <139303113+ALMOSTGOD1999@users.noreply.github.com>
2025-01-20 17:02:38 +0530

    l

--- client/service-worker.js
+++ client/service-worker.js
@@ -6,16 +6,15 @@ const urlsToCache = [
   "/client.js",
   "/assets/scissor.svg",
   "/assets/icons/icon-192x192.png",
-  "/assets/icons/icon-512x512.png"
+  "/assets/icons/icon-512x512.png",
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 // Install the service worker and cache resources
@@ -27,27 +26,50 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Fetch resources from the cache
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Update the cache when the service worker is activated
+// Activate Event: Cleanup old caches
 self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Deleting old cache:", cache);
+            return caches.delete(cache);
           }
         })
       );
     })
   );
 });
+
+// Fetch resources from the cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          if (event.request.mode === "navigate") {
+            return caches.match("/index.html");
+          }
+        })
+      );
+    })
+  );
+});
+
+// // Update the cache when the service worker is activated
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (!cacheWhitelist.includes(cacheName)) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
