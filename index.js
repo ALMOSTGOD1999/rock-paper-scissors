9a1359b536dc310c0b8e50876b3a479c154d496f
Rahul Mukherjee <139303113+ALMOSTGOD1999@users.noreply.github.com>
2025-01-20 20:43:32 +0530

    k

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function makeid(length) {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then((registration) => {
         console.log(
           "Service Worker registered with scope:",
